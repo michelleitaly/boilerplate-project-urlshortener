7c448b478c1557d2d5f52fc7a427bb927faab1f5
michelleitaly <michelle22289@gmail.com>
2022-04-12 22:57:57 +0200

    0412 notte

--- server.js
+++ server.js
@@ -3,11 +3,22 @@ let mongoose = require('mongoose')
 const express = require('express');
 const cors = require('cors');
 const app = express();
+mongoose.connect(process.env.SECRET_KEY, { useNewUrlParser: true, useUnifiedTopology: true });
 //console.log(process.env.SECRET_KEY)
-
 // Basic Configuration
 const port = process.env.PORT || 3000;
 //console.log(process.env.PORT)---undefined;
+
+//TEST MONGODB CONNECTION---
+const db = process.env.SECRET_KEY
+mongoose
+    .connect(db, { 
+        useNewUrlParser: true,
+        useCreateIndex: true
+      })
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
+//---TEST MONGODB CONNECTION
 
 app.use(cors());
 
@@ -18,6 +29,18 @@ let urlSchema = new mongoose.Schema({
 let Url = mongoose.model("url", urlSchema);
 let bodyParser = require("body-parser");
 //app.use(bodyParser.urlencoded({ extended: true }))
+/*var arrayOfUrl = [
+  {original: "Frankie", short: 74},
+  {original: "Sol", short: 76},
+ 
+];
+
+var createManyUrl = function(arrayOfUrl, done) {
+  Url.create(arrayOfUrl, function (err, url) {
+    if (err) return console.log(err);
+    done(null, url);
+  });
+};*/
 
 
 app.use('/public', express.static(`${process.cwd()}/public`));
@@ -43,13 +66,13 @@ app.post("/api/shorturl", bodyParser.urlencoded({ extended: false }), (req, res,
   let inputShort = 1;
   //find the bigest short_url number in database, if its  1/0? let new short_url =1, else short_url=shorturl++; if(!error)res.json(jsonObject),else 
   Url.findOne({})
-    .sort({ short: "desc " })
+    .sort({ short: "desc" })
     .exec((error, result) => {
       if (!error && result != undefined) {
         inputShort = result.short + 1;
       };
       if (!error) {
-        url.findOneAndUpdate({ original: req.body.url }, { original: req.body.url, short: inputShort }, { new: true, upsert: true }, (error, savedUrl) => {
+        Url.findOneAndUpdate({ original: req.body.url }, { original: req.body.url, short: inputShort }, { new: true, upsert: true }, (error, savedUrl) => {
           if (!error) {
             jsonObject["short_url"] = savedUrl.short;
             res.json(jsonObject)
@@ -57,7 +80,7 @@ app.post("/api/shorturl", bodyParser.urlencoded({ extended: false }), (req, res,
         })
       }
     })
-  jsonObject["short_url"] = short;
+  jsonObject["short_url"] = inputShort;
   // console.log(req.body)
   res.json(jsonObject)
 })
