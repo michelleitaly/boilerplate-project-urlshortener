0eca78f80fa60f35b5b601568bb3e3774af8af86
michelleitaly <michelle22289@gmail.com>
2022-04-15 22:31:20 +0200

    0415

--- server.js
+++ server.js
@@ -55,62 +55,102 @@ app.post(
   bodyParser.urlencoded({ extended: false }),
   (req, res) => {
     let inputUrl = req.body.url;
-    let urlRegex=new RegExp(/((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/)
-    if(!inputUrl.match(urlRegex)){
+    let urlRegex = new RegExp(
+      /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
+    );
+    if (!inputUrl.match(urlRegex)) {
       console.log("invalid url");
-     return  res.json({"error": "Invalid URL"})
-    };
-    let dataFound = {};
-    Url.findOne({ original_url: inputUrl }, (error, data) => {
-      if (error) {
-        console.log(error);
-      } else {
-        dataFound = data;
-        console.log("dataFound", dataFound)
-      }
-    });
+      return res.json({ error: "Invalid URL" });
+    }
     jsonObject["original_url"] = inputUrl;
     let inputShort = 1;
-    Url.findOne({})
-      .sort({ short_url: "desc" })
-      .exec((error, result) => {
-        if (!error && result !== null && dataFound === null) {
-          console.log("short + 1");
-          inputShort = result.short_url + 1;
-        }
-        if (!error && dataFound !== null) {
-          console.log("dataFound exist and going to print it",  dataFound.original_url, dataFound.short_url);
+
+    async function doWork() {
+      let dataFound = {};
+      console.log("befor await");
+      try {
+        //  await Url.findOne({ original_url: inputUrl }, (error, data) => {
+        //   if (error) {
+        //     console.log(error);
+        //   } else {
+        //     dataFound = data;
+        //     console.log("dataFound", dataFound);
+        //   }
+        // });
+
+        const dataExist = await Url.findOne({ original_url: inputUrl });
+
+        console.log("result----", dataExist);
+
+        //exec((error, result) => {
+        //   if (dataExist === null) {
+        //     const existingJsonObject = await Url.findOne({}).sort({
+        //       short_url: "desc",
+        //     });
+        //     console.log(
+        //       "This is the biggest short number Object----",
+        //       existingJsonObject
+        //     );
+
+        //   console.log("short + 1");
+        //   inputShort = existingJsonObject === null ? 1 : existingJsonObject.short_url + 1;
+        // }
+        if (dataExist !== null) {
+          console.log(
+            "dataFound exist and going to print it",
+            dataExist.original_url,
+            dataExist.short_url
+          );
           res.json({
-            original_url: dataFound.original_url,
-            short_url: dataFound.short_url,
+            original_url: dataExist.original_url,
+            short_url: dataExist.short_url,
           });
-          console.log("existed url printed", jsonObject)
-        } else if (!error) {
-          Url.findOneAndUpdate(
+          console.log("existed url printed", jsonObject);
+        } else {
+          const existingJsonObject = await Url.findOne({}).sort({
+            short_url: "desc",
+          });
+          console.log(
+            "This is the biggest short number Object----",
+            existingJsonObject
+          );
+
+        console.log("short + 1");
+        inputShort = existingJsonObject === null ? 1 : existingJsonObject.short_url + 1;
+         const newJsonObject= await Url.findOneAndUpdate(
             { original_url: inputUrl },
             { original_url: inputUrl, short_url: inputShort },
             { new: true, upsert: true },
-            (error, savedUrl) => {
-              console.log("I am going to creat a new url");
-              if (!error) {
-                jsonObject["short_url"] = savedUrl.short_url;
-                console.log("short", jsonObject)
-                res.json(jsonObject);
-              }
-            }
-          );
+            // (error, savedUrl) => {
+            //   console.log("I am going to creat a new url");
+            //   if (!error) {
+            //     jsonObject["short_url"] = savedUrl.short_url;
+            //     console.log("short", jsonObject);
+            //     res.json(jsonObject);
+            //   }
+            // }
+            );
+            console.log("I am going to creat a new url");
+              jsonObject["short_url"] = newJsonObject.short_url;
+              res.json(jsonObject);
+           
         }
-      });
+      } catch (error) {
+        console.error("error", error);
+      }
+    }
+
+    doWork();
   }
 );
-app.get("/api/shorturl/:num", (req, res)=>{
+app.get("/api/shorturl/:num", (req, res) => {
   let num = req.params.num;
-  Url.findOne({short_url: num},(error, result)=>{
-    if(!error && result !== null ){
-      res.redirect(result.original_url)
-      console.log("url redirected")
-    }else{
-      res.json("URL not found")
+  Url.findOne({ short_url: num }, (error, result) => {
+    if (!error && result !== null) {
+      res.redirect(result.original_url);
+      console.log("url redirected");
+    } else {
+      res.json("URL not found");
     }
-  })
-} )
+  });
+});
